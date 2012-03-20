db792e7e931ac94e5b5e45ec8742029bb05f1486
Bolo Michelin <bolo@lordalexworks.com>
2012-03-19 20:53:21 -0400

    add index

--- app.rb
+++ app.rb
@@ -2,11 +2,21 @@ require 'rubygems'
 require 'sinatra'
 require 'builder'
 
+#TO DO Recupere les données du SMS envoyé
 post '/' do
   builder do |xml|
     xml.instruct!
     xml.Response do 
       xml.Say("Bolo is awesome")
+    end
+  end
+end 
+
+get '/hello' do
+  builder do |xml|
+    xml.instruct!
+    xml.Response do 
+      xml.Say("Bolo from lordalexworks")
     end
   end
 end 
