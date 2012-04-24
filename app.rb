1b7ed3af0dc6bf073c9a7dea2d5853f64c8ed20c
Bolo Michelin <bolo@lordalexworks.com>
2012-04-24 09:30:04 -0400

    initial commit

--- app.rb
+++ app.rb
@@ -2,21 +2,6 @@ require 'rubygems'
 require 'sinatra'
 require 'builder'
 
-#TO DO Recupere les données du SMS envoyé
-post '/' do
-  builder do |xml|
-    xml.instruct!
-    xml.Response do 
-      xml.Say("Bolo is awesome")
-    end
-  end
-end 
-
-get '/hello' do
-  builder do |xml|
-    xml.instruct!
-    xml.Response do 
-      xml.Say("Bolo from lordalexworks")
-    end
-  end
-end 
+get '/' do
+  erb :index
+end
